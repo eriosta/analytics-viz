93b602c9f88af8daa8f613825c7609845dfb9d24
eriosta <eri@sienaspace.com>
2023-01-17 22:43:13 -0600

    Update while loop condition

--- week1/exercise2.py
+++ week1/exercise2.py
@@ -1,31 +1,31 @@
 import random
 import numpy as np
 
+guess = 0 
+randomNumber = random.randrange( 1, 21 )
+
 print( 'I have selected a random number between 1 and 20 inclusive. Guess my number.' )
 
 # The while loop, if, and elif conditions are incorrect. Replace True with the correct
 # relational expressions. 
-while True:
-    randomNumber = random.randrange( 1, 21 )
+while guess != randomNumber:
+    # randomNumber = random.randrange( 1, 21 )
     guess = int( input( 'What is your guess? ' ) )
 
     if guess == randomNumber:
         print( 'You are correct!' )
     elif guess > randomNumber:
-        if guess is not np.arange(1,21):
-            print(f"{guess} is outside of the range. Try a different number next time!")
+        if guess not in np.arange(1,21):
+            print(f"{guess} is outside of the range. Try a different number between 1 and 20!")
         else:
             print( 'Incorrect -- your guess is too high.' )
     elif guess < randomNumber:
-        if guess is not np.arange(1,21):
-            print(f"{guess} is outside of the range. Try a different number next time!")
+        if guess not in np.arange(1,21):
+            print(f"{guess} is outside of the range. Try a different number between 1 and 20!")
         else:
             print( 'Incorrect -- your guess is too low.' )
     else:
         print("Hmm... I don't understand this input. Try again!")
-        
     
     print( 'Game over!' )
-    print(f"The number was {randomNumber}")
     print( '###############' )
-    print( 'Restarting game' )

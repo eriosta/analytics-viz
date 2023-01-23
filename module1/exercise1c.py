29d60902391be1b24fd53e4a87c515fe06a1e587
eriosta <eri@sienaspace.com>
2023-01-22 20:43:08 -0600

    Finish 1c

--- module1/exercise1c.py
+++ module1/exercise1c.py
@@ -3,52 +3,97 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import random
 import math
+import sys, os
 
-# **Rules**
-
-# There are two players: PLAYER and BANKER. These are just names for the players. Each player has a regular six-sided die. 
-# In one round, each player will roll their die and attempt to get the highest value. The rules are:
-
-# - If either player rolls a six, the game stops. The player with the higher die value wins. If both dice values are the same, then it is a tie. 
-# - If neither player has a six in the initial roll, then a player may re-roll under these conditions:
-#     - If the BANKER initially rolled a 3 or below, the BANKER will re-roll their die. 
-#     - If the PLAYER initially rolled a 2 or below, the PLAYER will re-roll their die.
-#     - After this reroll, the game stops. The player with the higher die value wins. If both dice values are the same, then it is a tie. 
-
+# Roll die
 def rollDie( m ):
     die = random.randrange( 1, m + 1 ) 
     return die
 
+# Get key
 def get_key(val, dict):
     for key, value in dict.items():
         if val == value:
             return key
 
-def simulateGames( n, wins=None ):
+# Disable print
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore print
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+# Simulate games
+def simulateGames(n,verbose=False):
+
+    if verbose==True:
+        enablePrint()
+    else:
+        blockPrint()
+
+    wins = {'PLAYER':0, "BANKER":0, "TIE":0}
+
     for round in np.arange(1,n+1):
-        print(f"Round {round}")
 
         rolls = {'PLAYER':rollDie(6), "BANKER":rollDie(6)}
 
+        print(f"Round {round}")
+
         print(f"PLAYER rolled a %s and BANKER rolled a %s" % (rolls["PLAYER"],rolls["BANKER"]))
 
+        # If roll 6
         if rolls["PLAYER"] == 6 or rolls["BANKER"] == 6:
+            print(" ")
             print(f"Game stoped because {get_key(6,rolls)} rolled a 6")
-            break
-
-        elif rolls["PLAYER"] == rolls["BANKER"]:
+            if wins["BANKER"] > wins["PLAYER"]:
+                print("BANKER wins with", wins["BANKER"])
+            elif wins["PLAYER"] > wins["BANKER"]:
+                print("PLAYER wins with", wins["PLAYER"])
+            else:
+                print("No winner. It's a tie!")
+            print("###################")
             pass
+        # If tie
+        elif rolls["PLAYER"] == rolls["BANKER"]:
+            print("There is a tie!")
+            wins["TIE"] = wins["TIE"] + 1
 
-simulateGames(5)
-# Sample driver code
-# games = 200000 # Simulate 200000 games
-# outcomes = { 'player': 0, 'banker': 0, 'tie': 0 } # Dictionary to keep track of outcomes
-# simulateGames( games, outcomes ) # Call function to simulate the games
-# visualizeResults( games, outcomes ) # Call function to show bar chart
+        elif rolls["BANKER"] <= 3:
+            rolls["BANKER"] = rollDie(6)
+        
+        elif rolls["PLAYER"] <= 2:
+            rolls["PLAYER"] = rollDie(6)
 
+        if rolls["BANKER"] > rolls["PLAYER"]:
+            wins.update({"BANKER": wins["BANKER"] + 1})
 
-# >>> books = {}
-# >>> books['book'] = 3       
-# >>> books['book'] -= 1   
-# >>> books   
-# {'book': 2}   
+        elif rolls["PLAYER"] > rolls["BANKER"]:
+            wins.update({"PLAYER":wins["PLAYER"]+1})
+
+        print("###################")
+        print("PLAYER:",wins["PLAYER"])
+        print("BANKER:",wins["BANKER"])
+        print("###################")
+        print("")
+        print("")
+
+    enablePrint()
+    if wins["BANKER"] > wins["PLAYER"]:
+        print("BANKER wins with", wins["BANKER"])
+    elif wins["PLAYER"] > wins["BANKER"]:
+        print("PLAYER wins with", wins["PLAYER"])
+    else:
+        print("No winner. It's a tie!")
+    
+    return wins
+    
+def visualizeResults(n):
+    wins = simulateGames(n)
+    players = list(wins.keys())
+    scores = [wins[k] for k in players]
+    sns.barplot(x=players, y=scores)
+    plt.show()
+
+# Visualize games
+visualizeResults(n=200000)
